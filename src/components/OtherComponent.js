78990a8f99315c5f7fb33417daa7a65429d80c20
Niennis <estefania.osses.v@gmail.com>
2019-09-13 18:43:09 -0300

    First commit

---
+++ src/components/OtherComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+function OtherComponent() {
+  const getContent = () => {
+    return "Other component"
+  }
+
+  let content = getContent();
+
+  return(
+    <span> { content } </span>
+  );
+}
+
+export default OtherComponent;
